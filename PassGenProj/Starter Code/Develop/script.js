4cd05ee8fa50cb8539da5f07972fd7c4c6606728
ChapstickLover97 <118499830+ChapstickLover97@users.noreply.github.com>
2022-12-22 22:28:41 -0600

    Update script.js

--- PassGenProj/Starter Code/Develop/script.js
+++ PassGenProj/Starter Code/Develop/script.js
@@ -2,16 +2,77 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+
+function updateRangeValue(value) {
+  document.getElementById("rangeValue").innerText = value;
+};
+var generateBtn = document.getElementById("generate");
+
+generateBtn.addEventListener("click", function() {
+  writePassword(document.getElementById("rangeValue").innerText);
+});
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function writePassword(value) {
+  var includeUppercase = document.getElementById("uppercase").checked;
+  var includeLowercase = document.getElementById("lowercase").checked;
+  var includeNumeric = document.getElementById("numeric").checked;
+  var includeSpecial = document.getElementById("special").checked;
+
+  var password = "";
+
+  if (includeUppercase) {
+    var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    password = password + upperChars.charAt(Math.floor(Math.random() * upperChars.length));
+  }
+
+  if (includeLowercase) {
+    var lowerChars = "abcdefghijklmnopqrstuvwxyz";
+    password = password + lowerChars.charAt(Math.floor(Math.random() * lowerChars.length));
+  }
+
+  if (includeNumeric) {
+    var numChars = "0123456789";
+    password = password + numChars.charAt(Math.floor(Math.random() * numChars.length));
+  }
+
+  if (includeSpecial) {
+    var specialChars = "!@#$%^&*";
+    password = password + specialChars.charAt(Math.floor(Math.random() * specialChars.length));
+  }
+
+  for (var i = 0; i < value - 4; i++) {
+    var charSet;
+
+    if (includeUppercase && includeLowercase && includeNumeric && includeSpecial) {
+      charSet = upperChars + lowerChars + numChars + specialChars;
+    } else if (includeUppercase && includeLowercase && includeNumeric) {
+      charSet = upperChars + lowerChars + numChars;
+    } else if (includeUppercase && includeLowercase && includeSpecial) {
+      charSet = upperChars + lowerChars + specialChars;
+    } else if (includeUppercase && includeNumeric && includeSpecial) {
+      charSet = upperChars + numChars + specialChars;
+    } else if (includeLowercase && includeNumeric && includeSpecial) {
+      charSet = lowerChars + numChars + specialChars;
+    } else if (includeUppercase && includeNumeric) {
+      charSet = upperChars + numChars;
+    } else if (includeUppercase && includeSpecial) {
+      charSet = upperChars && specialChars;
+    } else if (includeLowercase && includeNumeric) {
+      charSet = lowerChars + numChars;
+    }
+  }
+
+
+  var passwordText = document.getElementById("password");
 
   passwordText.value = password;
+
+  function updateRangeValue(value) {
+    document.getElementById("rangeValue").innerText = value;
+  }
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
